ff4e5ad73e2d974188f4bde975091a35a8310c4d
Roman Ataman <root@atamanroman.dev>
2021-11-29 15:26:48 +0100

    Log CLI opts

--- src/gitlab_group_actions/core.clj
+++ src/gitlab_group_actions/core.clj
@@ -2,12 +2,14 @@
   (:require [gitlab-group-actions.cli :as cli]
             [gitlab-group-actions.pipelines :as pipelines]
             [gitlab-group-actions.tags :as tags]
-            [gitlab-group-actions.projects :as projects])
+            [gitlab-group-actions.projects :as projects]
+            [clojure.tools.logging :as log])
   (:gen-class))
 
 (defn -main [& raw] (let [opts (cli/parse-args raw)]
-                      (cond
-                        (= (:action opts) :exit) (System/exit (:exit-status opts))
-                        (= (:action opts) :start-pipeline) (doall (pipelines/start-pipelines opts (projects/get-projects opts)))
-                        (= (:action opts) :create-tag) (doall (tags/create-tags opts (projects/get-projects opts)))
-                        :else (throw (new IllegalStateException "Not implemented yet!")))))
+                      (do (log/debug "CLI args=" opts)
+                          (cond
+                            (= (:action opts) :exit) (System/exit (:exit-status opts))
+                            (= (:action opts) :start-pipeline) (doall (pipelines/start-pipelines opts (projects/get-projects opts)))
+                            (= (:action opts) :create-tag) (doall (tags/create-tags opts (projects/get-projects opts)))
+                            :else (throw (new IllegalStateException "Not implemented yet!"))))))
